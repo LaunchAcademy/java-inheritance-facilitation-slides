d6ef0a7ef10dbf1d02ad6ab65bda834437a550b1
KevinTranLA <kevin.tran@launchacademy.com>
2021-04-07 14:04:36 -0700

    Revert "adding update slides to public repo for students"
    This reverts commit 33326e2b33aac61cc79d5020aed214fc11a49feb.

--- src/main/java/Shape.java
+++
@@ -1,9 +0,0 @@
-public abstract class Shape {
-  public abstract double perimeter();
-
-  public abstract double area();
-
-  public void sharedMethod() {
-    System.out.println("I'm a shared method amongst all shapes!");
-  }
-}
